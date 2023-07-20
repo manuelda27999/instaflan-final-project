ef255ce7c0cdd76a3bd78ea66e8b283891d9149c
Manuel David Castillo <manuelda27999@gmail.com>
2023-07-20 19:38:17 +0200

    api and app, login and register

--- staff/manuel-david-castillo/fullstack/app/src/App.jsx
+++ staff/manuel-david-castillo/fullstack/app/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Register } from "./view/pages/Register";
+import { Login } from "./view/pages/Register";
+import { Home } from "./view/pages/Register";
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App() {
+  const [view, setView] = useState(sessionStorage.token ? 'home' : 'login')
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  const handleNavigateRegister = () => setView('register')
+  const handleNavigateToLogin = () => setView('login')
+  const handleNavigateHome = () => setView('home')
+
+  if (view === 'login') {
+    return <Login onRegisterClick={handleNavigateRegister} onLogin={handleNavigateHome} />
+  } else if (view === 'register') {
+    return <Register onLoginClick={handleNavigateToLogin} onRegister={handleNavigateToLogin} />
+  } else if (view === 'home') {
+    return <Home onLogout={handleNavigateToLogin} />
+  }
 }
-
-export default App
